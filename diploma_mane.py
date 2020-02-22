615b9dbecb54065492e31abf1263aba8c7135ad1
levinski <polinaprikhodova@gmail.com>
2020-02-22 03:45:51 +0300

    added clint module, new exception checking

--- diploma_mane.py
+++ diploma_mane.py
@@ -2,8 +2,9 @@ from urllib.parse import urlencode
 import requests
 import time
 import json
-from clint.textui import colored, puts
+from extramodule.clint.textui import colored, puts
 import random
+
 
 #adress = 'eshmargunov'
 app_id = 7315243
@@ -19,14 +20,18 @@ params = {
 token = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
 
 def getUserID(adress):
-    response = requests.get(
-        'https://api.vk.com/method/users.get',
-        {'user_ids': adress,
-        'access_token': token,
-        'v': 5.103}
-        )
-    id = response.json()['response'][0]['id']
-    return id
+    try:
+        response = requests.get(
+            'https://api.vk.com/method/users.get',
+            {'user_ids': adress,
+            'access_token': token,
+            'v': 5.103}
+            )
+        id = response.json()['response'][0]['id']
+    except KeyError:
+        print('Invalid id!')
+    else:
+        return id
 
 def getFriendsIDs(id):
     response = requests.get(
@@ -36,9 +41,18 @@ def getFriendsIDs(id):
         'v': 5.103}
         )
     friends = response.json()['response']['items']
+    friends_sep = []
     friends = [str(x) for x in friends]
-    friends = ','.join(friends)
-    return friends
+    while friends:
+        if len(friends) > 300:
+            friends_sep.append(','.join(friends[:301]))
+            friends = friends[301:]
+        else:
+            friends_sep.append(','.join(friends))
+            break
+    friends = ','.join(friends_sep)
+    return friends_sep
+
 
 def getUserGroups(id):
     response = requests.get(
@@ -51,31 +65,40 @@ def getUserGroups(id):
     return groups
 
 def getXorGroups(groups, friends):
-    res = user_groups[:]
-    for group in user_groups:
-        puts(getattr(colored, random.choice(colored.COLORS))('\rProcessing...'), newline=False)
-        while True:
-            try:
-                response = requests.get(
-                    'https://api.vk.com/method/groups.isMember',
-                    {'access_token': token,
-                    'v': 5.103,
-                    'user_ids': friends,
-                    'group_id': group,
-                    'extended': 1
-                    })
-                responses = response.json()['response']
-                for resp in responses:
-                    if resp['member'] == 1:
-                        res.remove(group)
+    if friends == []:
+        return groups
+    else:
+        res = user_groups[:]
+        for group in user_groups:
+            puts(getattr(colored, random.choice(colored.COLORS))('\rProcessing...'), newline=False)
+            while True:
+                try:
+                    for friends in friends_sep:
+                        response = requests.get(
+                            'https://api.vk.com/method/groups.isMember',
+                            {'access_token': token,
+                            'v': 5.103,
+                            'user_ids': friends,
+                            'group_id': group,
+                            'extended': 1
+                            })
+                        responses = response.json()['response']
+                        for resp in responses:
+                            if resp['member'] == 1:
+                                if not group in res:
+                                    break
+                                res.remove(group)
+                                break
+                except KeyError:
+                    if response.json()['error']['error_code'] == 6:
+                        time.sleep(0.1)
+                    else:
+                        print('Another exc')
                         break
-            except KeyError:
-                time.sleep(0.4)
-            else:
-                break
-                responses = response.json()['response']
-
-    return res
+                else:
+                    break
+                    responses = response.json()['response']
+        return res
 
 def dumper(filename='groups.json'):
     with open(filename, 'w') as f:
@@ -97,8 +120,18 @@ def dumper(filename='groups.json'):
                     dictionary['gid'] = r['id']
                     dictionary['members_count'] = r['members_count']
                     list.append(dictionary)
-                except KeyError:
-                    time.sleep(0.4)
+                except Exception as exc:
+                    if 'error' in response.json() and response.json()['error']['error_code'] == 6:
+                        time.sleep(0.1)
+                    elif response.json()['response'][0]['deactivated'] == 'banned':
+                        dictionary['name'] = r['name']
+                        dictionary['gid'] = r['id']
+                        dictionary['members_count'] = 'banned'
+                        list.append(dictionary)
+                        break
+                    else:
+                        print('Unknown exc', exc)
+                        break
                 else:
                     break
         json.dump(list, f, ensure_ascii=False, indent = 2)
@@ -106,8 +139,11 @@ def dumper(filename='groups.json'):
 if __name__ == '__main__':
     adress = input('Enter short adress: ')
     user_id = getUserID(adress)
-    friends = getFriendsIDs(user_id)
-    user_groups = getUserGroups(user_id)
-    res = getXorGroups(user_groups, friends)
-    dumper()
-    print('\nDone!')
+    #print(user_id)
+    if user_id:
+        friends_sep = getFriendsIDs(user_id)
+        user_groups = getUserGroups(user_id)
+        #print(user_groups)
+        res = getXorGroups(user_groups, friends_sep)
+        dumper()
+        print('\nDone!')
